886f53427fc32da517dd584995d8110337b38cc8
Jesse Angelo <jesse@jesseangelo.com>
2014-11-08 23:45:25 -0500

    Added infinite tasks

--- Perezoso.js
+++ Perezoso.js
@@ -3,17 +3,28 @@ function displayMessage(msg) {
 }
 
 var Perezoso = (function() {
-  var tasks = [],
-  intervalID = null; 
+  var tasksTimed = [],
+      tasksInfinite = [],
+      isRunning = false,
+      intervalID = null; 
   return {
     add: function(w, f, p) {
-      var self = this;
+      this.addTimed(w, f, p);
+    },
+    addTimed: function(w, f, p) {
       var d = new Date();
       var t = d.getTime();
       var myW = t + w;
-      //document.write("INIT: Now is: " + t + " waiting for: " + myW + "....\n\n");
-      tasks.push({start: t, when: myW, func: f, param: p, infinite: false});
+      tasksTimed.push({start: t, when: myW, func: f, param: p });
       //console.log("added: " + w + " f: " + f + " myID: " + o);
+      this.init();
+    },
+    addInfinite: function(w, f, p) {
+      var d = new Date();
+      var t = d.getTime();
+      var myW = t + w;
+      tasksInfinite.push({interval: w, when: myW, func: f, param: p });
+      //console.log("added Infinite: " + w + " f: " + f);
       this.init();
     },
     update: function() {
@@ -23,35 +34,51 @@ var Perezoso = (function() {
     init: function(){
       if(intervalID === null) {
         intervalID = window.setInterval(this.update, 100);
+        isRunning = true;
       }
     },
-    findTime: function() {
-      if(tasks.length) {
-        for(var q = 0; q < tasks.length; q++) {
+    findTime: function() 
+    {
+      if(tasksTimed.length) 
+      {
+        for(var q = 0; q < tasksTimed.length; q++) 
+        {
           var d = new Date();
           var myT = d.getTime();
-          var myW = tasks[q].when;
+          var myW = tasksTimed[q].when;
           
-          if((myW-myT) < 0) {
-            tasks[q].func(tasks[q].param);
-            if(tasks[q].infinite) {
-              //Perezoso.add(2000, true, (function(){ document.write("You win!! <br>")}));
-              //tasks.push({start: t, when: myW, func: f, param: p, infinite: i});
-              //
-            } else {
-              tasks[q] = null;
-            }
+          if((myW-myT) < 0) 
+          {
+            tasksTimed[q].func(tasksTimed[q].param);
+            tasksTimed[q] = null;
+          }
+        } 
+      }
+      if(tasksInfinite.length) 
+      {
+        for(var q = 0; q < tasksInfinite.length; q++) 
+        {
+          var d = new Date();
+          var myT = d.getTime();
+          var myW = tasksInfinite[q].when;
+          
+          if((myW-myT) < 0) 
+          {
+            tasksInfinite[q].func(tasksInfinite[q].param);
+            var newT = myT + tasksInfinite[q].interval;
+            console.log(newT);
+            tasksInfinite[q].when = d.getTime() + tasksInfinite[q].interval;
           }
         } 
       }
     },
     cleanList: function() {
       var newArray = [];
-       for(var q = 0; q < tasks.length; q++) {
-          if(tasks[q] != null) { newArray.push(tasks[q]); }
+       for(var q = 0; q < tasksTimed.length; q++) {
+          if(tasksTimed[q] != null) { newArray.push(tasksTimed[q]); }
        }
-       tasks = newArray;
-       if(newArray.length == 0) {
+       tasksTimed = newArray;
+       if(newArray.length == 0 && tasksInfinite.length == 0) {
           clearInterval(intervalID);
           intervalID = null;
        }
@@ -60,7 +87,8 @@ var Perezoso = (function() {
 })();
 
 /*
-Perezoso.add(2000, (function(){ document.write("You win!! <br>")}));
+Perezoso.addTimed(2000, (function(){ document.write("You win!! <br>")}));
 Perezoso.add(2500, (function(){ document.write("You win again!! <br>")}));
 Perezoso.add(3500, displayMessage, "I <3 JS");
+Perezoso.addInfinite(3500, (function(){ document.write("Infinite! <br>")}));
 */
