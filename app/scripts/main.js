6154ca5b5878d71ef98e1ee9042f4f80f4b8cccd
Jesse Angelo <jesse@jesseangelo.com>
2017-03-12 14:28:16 -0400

    Test stuff

---
+++ app/scripts/main.js
@@ -0,0 +1,9 @@
+function displayMessage(msg) {
+  console.log(msg);
+}
+
+
+//PEREZOSO.addTimed(2000, (function(){ document.write("You win!! <br>")}));
+//PEREZOSO.add(2500, (function(){ document.write("You win again!! <br>")}));
+//PEREZOSO.add(3500, displayMessage, "I <3 JS");
+//PEREZOSO.addInfinite(3500, (function(){ document.write("Infinite! <br>")}));
