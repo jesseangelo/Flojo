e1d7cc846def96e431e45631af27602cb57d31fd
Jesse Angelo <jesseangelo@Jesses-MBP.fios-router.home>
2017-03-19 17:32:42 -0400

    addCounted

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,8 +1,5 @@
-function displayMessage(msg) {
-  console.log(msg);
-}
 
-
+/*
 var k1 = PEREZOSO.addTimed(2000, function(){ document.write("You win!! <br>")});
 var k = PEREZOSO.addTimed(2500, function(){ document.write("You win again!! <br>")});
 console.log(k1);
@@ -14,3 +11,4 @@ var inf = PEREZOSO.addInfinite(3500, function(){ document.write("Infinite! <br>"
 PEREZOSO.addTimed(15000, function (){
   PEREZOSO.removeTask(inf)
 })
+*/
