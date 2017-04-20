02d8b3b17f9e8edd9c33958a8fe5d632e7dd0464
Jesse Angelo <jesseangelo@Jesses-MBP.fios-router.home>
2017-04-20 00:05:33 -0400

    super rough fade out, cleaned up some extra variables, more error checking

--- app/scripts/lib/Flojo.js
+++ app/scripts/lib/Flojo.js
@@ -47,7 +47,7 @@ var FLOJO = (function() {
   var getNewTaskId = function() {
         //console.log('currentTaskId: ' + currentTaskId)
         init();
-        return (currentTaskId++);
+        return currentTaskId++;
       },
       init = function() {
         if(intervalID === null) {
@@ -56,6 +56,7 @@ var FLOJO = (function() {
         }
       },
       update = function() {
+
         findTime();
 
         cleanList();
@@ -67,44 +68,47 @@ var FLOJO = (function() {
         }
       },
       getNewWhen = function(interval) {
-        var d = new Date();
         return new Date().getTime() + interval;
       },
       findTime = function() {
-        var d = new Date();
-        var myT = new Date().getTime();
 
         tasks.forEach(function(task, index) {
+          
           if(task == null) return;
-          var myW = task.when;
-          if((myW-myT) < 0)
+          
+          if((task.when - new Date().getTime()) < 0)
           {
             task.func(task.param);
 
             switch(task.type)
             {
               case 1:     //timed
+
                 tasks[index] = null;
                 break;
+              
               case 2:     //count
+              
                 task.count--;
                 if(task.count > 0) {
-                  var newT = myT + task.interval;
+                  var newT = new Date().getTime() + task.interval;
                   //if(_debug) console.log(newT);
                   task.when = getNewWhen(task.interval);
                 } else {
                   tasks[index] = null;
                 }
                 break;
+              
               case 3:     //infinite
-                var newT = myT + task.interval;
+              
+                var newT = new Date().getTime() + task.interval;
                 task.when = getNewWhen(task.interval);
                 break;
+              
               default: 
                 break;
-            }
 
-            
+            }
           }
         });
       },
@@ -116,13 +120,15 @@ var FLOJO = (function() {
         tasks = newArray;
       },
       kill = function(id) {
+        if(tasks.length == 0) { return false; }
+
         tasks.forEach(function(task, index) {
           if(task != null) {
-            var myId = task.id;
-            if(myId == id)
+            if(task.id == id)
             {
               tasks[index] = null;
               if(_debug) { console.log('task killed: ' + id) }
+                return true;
             }
           }
         });
@@ -140,17 +146,15 @@ var FLOJO = (function() {
   return {
 
     timed: function(when, myFunc, myParam) {
-      var d = new Date();
-
       if(when < 0) { throw new Error("When needs to be positive")}
 
       var task = new Task(
-        getNewTaskId(),         //ID
-        1,                      //type
-        d.getTime(),            //start time
-        d.getTime() + when,     //end time
-        myFunc,                 //function
-        myParam)                //parameter
+        getNewTaskId(),               //ID
+        1,                            //type
+        new Date().getTime(),         //start time
+        new Date().getTime() + when,  //end time
+        myFunc,                       //function
+        myParam)                      //parameter
 
       tasks.push(task);
 
@@ -158,16 +162,14 @@ var FLOJO = (function() {
 
       //console.log('timed Id: ' + myId)
       return task.id;
-
     },
     after: function(id, when, myFunc, myParam) {
-      //console.log(id)
-      var d = new Date();
-      
+      if(when < 0) { throw new Error("When needs to be positive")}
+
       var task = new Task(
         getNewTaskId(),
         1,
-        d.getTime(),            //start time
+        new Date().getTime(),            //start time
         getTaskFromId(id).when + when,     //end time
         myFunc,                 //function
         myParam)                //parameter
@@ -182,15 +184,15 @@ var FLOJO = (function() {
     // },
 
     counted: function(when, count, myFunc, myParam) {
-      //error checking for values
-      var d = new Date();
+      if(when < 0) { throw new Error("When needs to be positive")}
+      if(count < 0) { throw new Error("Count needs to be positive")}
 
       var task = new Counted(
           getNewTaskId(),         //ID
           2,                      //type
           when,                   //when
-          d.getTime(),            //start time
-          d.getTime() + when,     //end time
+          new Date().getTime(),            //start time
+          new Date().getTime() + when,     //end time
           count,
           myFunc,                 //function
           myParam)                //parameter
@@ -204,14 +206,14 @@ var FLOJO = (function() {
     },
 
     infinite: function(when, myFunc, myParam) {
-      var d = new Date();
+      if(when < 0) { throw new Error("When needs to be positive")}
       
       var task = new Infinite(
           getNewTaskId(),         //ID
           3,                      //type
           when,                   //when   
-          d.getTime(),            //start time
-          d.getTime() + when,     //end time
+          new Date().getTime(),            //start time
+          new Date().getTime() + when,     //end time
           myFunc,                 //function
           myParam)                //parameter
 
@@ -223,7 +225,11 @@ var FLOJO = (function() {
     },
     remove: function (id) {
       if(_debug) console.log("Killing task " + id)
-      kill(id);
+      var dead = kill(id);
+      if(dead == false){
+        throw new Error("task id: " + id + " cannot be found");
+      }
+
     },
     waitFor: function(obj, prop, value, myFunc) {
       var f = myFunc;
@@ -281,22 +287,39 @@ var F = function(arg) {
     remove: function() {
       el.parentNode.removeChild(el);
     },
-    opacity: function(o) {
-      var els = getEls(arg)
-      els.forEach(function(element) {
-         
-          element.style.opacity = o;
-        
-      });
-    },
     hasClass: function(c) {
       if (el.classList)
         el.classList.contains(c);
       else
         new RegExp('(^| )' + c + '( |$)', 'gi').test(el.c);
+    },
+
+    opacity: function(o) {
+      var els = getEls(arg)
+      els.forEach(function(element) {
+        element.style.opacity = o; 
+      });
+    },
+    fadeOut: function(time) {
+      var els = getEls(arg)
+      var loops = Math.round(time/60);
+
+      //this still needs work for sure
+      for(var k = 0; k < loops; k++) {
+        FLOJO.timed(k * 60, function(){
+          console.log(k/loops)
+          els.forEach(function(element) {
+            element.style.opacity = k/loops; 
+          });
+        })
+      } 
+    },
+    fadeIn: function(time) {
+
     }
-    /*
-    */
+    
+    //color
+
     //animate
     //randomNum?
   }
