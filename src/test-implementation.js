2fa6cac3c231958e0ab6123c10fb88a26ae3fb08
Jesse Angelo <jesse.angelo@jetblue.com>
2018-11-25 13:31:26 -0500

    Some testing

--- src/test-implementation.js
+++ src/test-implementation.js
@@ -8,3 +8,12 @@ console.log("hello from singleton! version:" + flojo.APP_VERSION);
 flojo.timed(1000, () => {
   console.log('IT FREAKING WORKS')
 })
+
+
+flojo.infinite(1500, () => {
+  console.log('infinite')
+})
+
+flojo.counted(1200, 5, () => {
+  console.log('counting to 5')
+})
