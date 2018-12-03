9d8f248fa45c65c0b92e1abaac4d0f736454e077
Jesse Angelo <jesse.angelo@jetblue.com>
2018-12-03 14:14:15 -0500

    Coverage

--- src/core.spec.js
+++ src/core.spec.js
@@ -6,4 +6,27 @@ describe('Core tests', () => {
 
     expect(c).toBeTruthy();
   });
+
+  it('should set time on init if not running', () => {
+    let c = new core();
+    let updateSpy = spyOn(c, "update");
+    jest.spyOn(window, 'requestAnimationFrame').mockImplementation(cb => cb());
+
+    expect(c.isRunning).toEqual(false);
+
+    c.init();
+
+    expect(c.isRunning).toEqual(true);
+    // jest.useFakeTimers()
+    // jest.advanceTimersByTime(250);
+
+    // expect(updateSpy).toHaveBeenCalled();
+  });
+
+  it('should add a task to the array of tasks', () => {
+    let c = new core();
+    c.add({});
+    expect(c.tasks.length).toEqual(1);
+  })
+
 });
